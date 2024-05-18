1f3de29730b8c47b467c646be49e372f34f62d17
= <muhammedzohidcr7@gmil.com>
2024-05-18 04:07:43 -0700

    updat

--- main/views.py
+++ main/views.py
@@ -13,7 +13,7 @@ from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-@login_required(login_url="logi")
+@login_required(login_url="login")
 def index(request):
     return render(request,"base.html")
 
